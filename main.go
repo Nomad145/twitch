006882d7290059b269e75596694921ee7dd117b4
Michael Phillips <michaeljoelphillips@gmail.com>
2018-09-30 03:48:29 -0500

    Remove secrets.

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 )
 
 const client_id = "g0m4aoe1qgv0lqais31yp27yzvw603"
-const client_secret = "xhk8oay2vvhmpc63ingdsbavix2j7k"
-const access_token = "twm4mh8qim2hawaf13jv8ia5i8eay5"
 
 var client = twitch.NewClient(client_id)
 var segments = make(chan string, 1024)
